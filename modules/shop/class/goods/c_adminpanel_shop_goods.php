94b443de6f4ba43434c0b5f764f389bbc7227894
Drxlog <rowsinthemrc@gmail.com>
2018-08-03 22:38:12 +0300

    0.0.3

--- modules/shop/class/goods/c_adminpanel_shop_goods.php
+++ modules/shop/class/goods/c_adminpanel_shop_goods.php
@@ -12,10 +12,12 @@ class C_adminpanel_shop_goods
 		$Connect = $SHT->query('SELECT name,img FROM goods');
 		while ($row = $Connect->fetch()) {
     		echo
-    
     		'<div class="ad_shop_good_wrapper">'.
     		'<img src="http://zebra-cafe.test/modules/shop/assets/img/jpg/goods/'.$row['img'].'" alt="">'.
-    		'<div class="ad_shop_good_trash"><a href="#"><img src="http://zebra-cafe.test/modules/shop/assets/img/svg/trash.svg" alt=""></a></div>'.
+    		'<div class="ad_shop_good_icons">
+    			<a href="#"><img src="http://zebra-cafe.test/modules/shop/assets/img/svg/pen.svg" alt=""></a>
+				<a href="#" id="ad_shop_good_pen"><img src="http://zebra-cafe.test/modules/shop/assets/img/svg/trash.svg" alt=""></a>
+    		</div>'.
     		'<div class="ad_shop_good_name"><p>'.$row['name'].'</p></div>'.
     		'</div>'
     		;
